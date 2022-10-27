f2bb456e9667507abf6f49414aad026c9d2a5ecc
unknown <ak-rakib>
2022-10-27 04:15:17 -0700

    modify data

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const cors = require('cors');
+
+app.use(cors());
 const categories = require('./Data/Category.json');
 const courses = require('./Data/Courses.json');
 
-app.get('/courses-categories',(req,res)=> {
+app.get('/courses-category',(req,res)=> {
     res.send(categories);
 })
 
@@ -12,9 +15,19 @@ app.get('/courses', (req, res)=> {
     res.send(courses);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/categories/:id', (req, res)=> {
+    const id = req.params.id;
+    const categoriesId = categories.find(category => category.id === id)
+    res.send(categoriesId);
+});
+
+
+app.get('/category/:id', (req, res)=>{
+    const id = req.params.id;
+    const course = courses.find(course => course.category_id === id);
+    res.send(course);
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
